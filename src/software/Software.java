4a441a7122b2d4a5b052bcaae9ca0494009a3cc1
sharante <sharante@shernet.sheridancollege.ca>
2020-01-20 13:14:49 -0500

    Practicing

---
+++ src/software/Software.java
@@ -0,0 +1,16 @@
+package software;
+
+/**
+ *
+ * @author Rich Smith at ZenOfProgramming.com
+ */
+public class Software 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+}
